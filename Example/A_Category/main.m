9ff3d146e2ca4e8c879afeff31426f4a7a5c57bf
李伟国 <liweiguo1215@163.com>
2020-07-05 16:00:31 +0800

    Initial commit

---
+++ Example/A_Category/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  A_Category
+//
+//  Created by 17710114190@163.com on 07/05/2020.
+//  Copyright (c) 2020 17710114190@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "WGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WGAppDelegate class]));
+    }
+}
